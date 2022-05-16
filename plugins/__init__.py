878850579eec8e58174878ab0d30d6f823ef880c
Akash Manna <akashmanna2099@gmail.com>
2022-05-16 18:46:18 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -169,12 +169,16 @@ class Hybrique(pcbnew.ActionPlugin):
             import webbrowser
             if SYSTEM_PLATFORM == 'Windows':
                 import ctypes
+
                 STYLE_YESNO = 0x04
+                WS_EX_TOPMOST = 0x40000
                 ICON_EXLAIM=0x30
-                result = ctypes.windll.user32.MessageBoxW(0, "Please download and install the standalone plugin!\nDo you want to proceed to the webpage ?", "App not found!", STYLE_YESNO | ICON_EXLAIM)
+
+                result = ctypes.windll.user32.MessageBoxA(0, "Please download and install the standalone plugin!\nDo you want to proceed to the webpage ?", "App not found!", STYLE_YESNO | WS_EX_TOPMOST | ICON_EXLAIM)
+
                 if (result == 6):
                     webbrowser.open_new_tab(HYBRIQUE_PRODUCT_PAGE)
-                elif (result == 7):
+                else:
                     pass
             elif SYSTEM_PLATFORM == "Darwin":
                 import wx
